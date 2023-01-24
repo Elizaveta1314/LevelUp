b8a614d185cb11c74f4c36ce8814518f41dc4136
liza <liza.haritonova@mail.ru>
2023-01-25 04:11:35 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// slider
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:false,
+		dots:true,
+		adaptiveHeight: true,
+		speed:1000,
+		autoplay:true,
+		autoplaySpeed:100000,
+
+	});
+});
